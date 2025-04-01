7c97fc550f8a8c5960d7b24eb3f86f5a4f454450
WAKISAISABELLE <wakisaisabelle@gmail.com>
2025-04-01 22:59:46 +0300

    working on the sales-agent dashboard

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import SalesAgent from '../../sales-agent/Dashboard.js';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin" element={<SalesAgent/>} />
+       
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
