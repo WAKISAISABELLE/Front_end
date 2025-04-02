a7684fc1fc9e810802159f5dcdcda5d03b50e905
Ssenyonjo-Andrew <ande.ssenyonjo@gmail.com>
2025-04-02 14:48:55 +0300

    created a login file

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import SalesAgentDashboard from './pages/sales-agent/Dashboard.js';
-
+import Login from './auth/login.js';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/sales-agent/:branch" element={<SalesAgentDashboard/>} />
-       
+        <Route path="/" element={<Login />} />
+        {/* Add other routes here */}
       </Routes>
     </BrowserRouter>
   );
